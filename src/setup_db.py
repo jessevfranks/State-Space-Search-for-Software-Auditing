a2a8ea81bc0f6ffcfdd076f7cb68c057d773cade
Jesse Franks <jessevfranks@gmail.com>
2025-11-16 17:44:23 -0500

    Setup db

--- src/setup_db.py
+++ src/setup_db.py
@@ -8,7 +8,7 @@ cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
-    password TEXT NOT NULL,
+    password TEXT NOT NULL
 )
 ''')
 
