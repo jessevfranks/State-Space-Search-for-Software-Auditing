1aed3d93b2e64e72e3b9f43b6cdb4878c2a4896f
Jesse Franks <jessevfranks@gmail.com>
2025-11-16 20:39:09 -0500

    Add root EP and create DB automatically

--- src/setup_db.py
+++ src/setup_db.py
@@ -1,27 +1,36 @@
 import sqlite3
 
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
+def setup_db():
+    conn = None
+    try:
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
 
-# Create user table
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-)
-''')
+        # Create user table
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+        ''')
 
-# Insert the "admin" user
-cursor.execute(
-    "INSERT INTO users (username, password) VALUES (?, ?)",
-    ('admin', 'very_coMplEX_passWoRd_123')
-)
+        # Insert the "admin" user
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            ('admin', 'very_coMplEX_passWoRd_123')
+        )
 
-conn.commit()
-conn.close()
+        conn.commit()
+        conn.close()
 
-print("users.db created successfully with admin user")
-
-
-
+        print("users.db created successfully with admin user")
+        return True
+    except sqlite3.OperationalError as e:
+        print(e)
+        return False
+    finally:
+        if conn:
+            conn.close()
--- src/victim_app.py
+++ src/victim_app.py
@@ -1,9 +1,12 @@
 import sqlite3
 from flask import Flask, request, render_template_string
+from setup_db import setup_db
 
 app = Flask(__name__)
 
 # HTML templates for responses
+root_page = "<h1> Use /login to view login screen </h1>"
+
 login_page = '''
     <form method="POST">
         Username: <input type="text" name="username"><br>
@@ -14,12 +17,21 @@ login_page = '''
 
 welcome_page = "<h1>Welcome, admin!</h1>"
 invalid_page = "<h1>Invalid username or password.</h1>"
+internal_server_error = "<h1>Internal server error.</h1>"
+
+@app.route('/', methods=['GET'])
+def root():
+    return render_template_string(root_page)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
     if request.method == 'GET':
-        return render_template_string(login_page)
+        return render_template_string(login_page), 200
+
+    if not setup_db():
+        return render_template_string(internal_server_error), 500
 
     username = request.form.get('username')
     password = request.form.get('password')
@@ -50,3 +62,6 @@ def login():
     finally:
         if conn:
             conn.close()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
