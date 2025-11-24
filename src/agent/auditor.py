44077f54b4dc9f02ae3a42177d9f1207fb573f77
Jesse Franks <jessevfranks@gmail.com>
2025-11-23 20:01:23 -0500

    BFS

--- src/agent/auditor.py
+++ src/agent/auditor.py
@@ -1,30 +1,35 @@
+from collections import deque
+
 from helpers import *
 import heapq
-import State
+from State import *
 
-def run_audit(start_username, start_password):
+def run_informed_audit(start_username, start_password):
     """
     Implementation of the A* algorithm
     """
     # heap to hold States needing to be explored
-    open = []
+    open_states = []
 
     # set to hold explored tuples of (username, password)
     explored = set()
 
     first_state = State(
         heuristic_cost(start_username, start_password),
-        path_cost(start_username, start_password),
+        0,
         start_username,
         start_password,
         None,
-        [],
     )
-    heapq.heappush(open, first_state)
+    heapq.heappush(open_states, first_state)
     explored.add((start_username, start_password))
 
-    while open:
-        curr_state = open.pop()
+    while open_states:
+        curr_state = heapq.heappop(open_states)
+        print(f"""Exploring state with:
+              username: {curr_state.username}
+              password: {curr_state.password}
+              cost: {curr_state.total_cost}""")
 
         if curr_state.h_cost == 0:
             print(f"Achieved goal state with username: {curr_state.username} and password: {curr_state.password}")
@@ -39,7 +44,7 @@ def run_audit(start_username, start_password):
             explored.add((new_username, new_password))
 
             h_cost = heuristic_cost(new_username, new_password)
-            g_cost = path_cost(new_username, new_password)
+            g_cost = curr_state.g_cost + 1
 
             new_state = State(
                 h_cost,
@@ -47,9 +52,59 @@ def run_audit(start_username, start_password):
                 new_username,
                 new_password,
                 curr_state,
-                curr_state.path.append(new_username),
             )
-            heapq.heappush(open, new_state)
+            heapq.heappush(open_states, new_state)
+
+    print("Could not reach goal state")
+    return None
+
+def run_uninformed_audit(start_username, start_password):
+    """
+    BFS implementations
+    """
+    # queue to hold States needing to be explored
+    open_states = deque()
+
+    # set to hold explored tuples of (username, password)
+    explored = set()
+
+    first_state = State(
+        fake_heuristic(start_username, start_password),
+        0,
+        start_username,
+        start_password,
+        None,
+    )
+    open_states.append(first_state)
+    explored.add((start_username, start_password))
+
+    while open_states:
+        curr_state = open_states.popleft()
+        print(f"""Exploring state with:
+                  username: {curr_state.username}
+                  password: {curr_state.password}
+                  cost: {curr_state.total_cost}""")
+
+        if curr_state.h_cost == 0:
+            print(f"Achieved goal state with username: {curr_state.username} and password: {curr_state.password}")
+            return curr_state
+
+        mutations = generate_mutations(curr_state)
+
+        for new_username, new_password in mutations:
+            if (new_username, new_password) in explored:
+                continue
+
+            explored.add((new_username, new_password))
+
+            new_state = State(
+                fake_heuristic(new_username, new_password),
+                0,
+                new_username,
+                new_password,
+                curr_state,
+            )
+            open_states.append(new_state)
 
     print("Could not reach goal state")
     return None
